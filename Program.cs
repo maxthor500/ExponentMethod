552c68d72860a1270b47585a3c87740a8ac4a2fc
maxthor500 <iraubuntu@gmail.com>
2022-06-06 22:57:04 +0100

    Create a program that given two numbers get the first number as base and the second as exponent

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetPower(3, 0));
+            Console.WriteLine(GetPower(7, 1));
+            Console.WriteLine(GetPower(6, 5));
+            Console.ReadLine();
+        }
+
+        static int GetPower(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for(int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+            return result;
         }
     }
 }
